f6c1e6543a901769299423c6454475e1e751858d
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-08 17:35:02 +0200

    install ruby via rbenv and default to 2.3.1

--- recipes/sspec-install.rb
+++ recipes/sspec-install.rb
@@ -1,2 +1,30 @@
 include_recipe 'apt'
 include_recipe 'git'
+
+package [ 'git-core', 'curl', 'zlib1g-dev', 'build-essential',
+          'libssl-dev', 'libreadline-dev', 'libyaml-dev', 'libsqlite3-dev',
+          'sqlite3', 'libxml2-dev', 'libxslt1-dev', 'libcurl4-openssl-dev',
+          'python-software-properties', 'libffi-dev' ] do
+  action :install
+end
+
+script 'install_ruby_rbenv' do
+  code <<-EOH
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    exec $SHELL
+
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+    exec $SHELL
+
+    rbenv install 2.3.1
+    rbenv global 2.3.1
+    ruby -v
+    EOH
+end
+
+execute 'install_bundler' do
+  command 'gem install bundler'
+end
