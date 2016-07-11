ecf5f4a511530b9baf033ff4823f488ddc0b9510
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-11 15:14:16 +0200

    ruby version 2.2.2

--- recipes/sspec-install.rb
+++ recipes/sspec-install.rb
@@ -27,8 +27,8 @@ end
 
 bash 'install_ruby' do
   code <<-EOH
-    ~/.rbenv/bin/rbenv install 2.3.1
-    ~/.rbenv/bin/rbenv global 2.3.1
+    ~/.rbenv/bin/rbenv install 2.2.2
+    ~/.rbenv/bin/rbenv global 2.2.2
     ruby -v
     EOH
 end
