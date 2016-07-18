fae9e84743d77eafdf41fb77c2953ba84934443f
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 16:53:57 +0200

    spec files in files

---
+++ files/default/spec/sample_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe package('git'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('git'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('ssh'), :if => os[:family] == 'redhat' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('ssh'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
