d4f5ac9c7f3d58709317849ce30fa5648c31f0c0
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 16:47:12 +0200

    spec files in files

--- recipes/healthcheck.rb
+++ recipes/healthcheck.rb
@@ -1,4 +1,4 @@
-remote_directory "#{node['serverspec']['dest_spec_path']}/spec" do
+remote_file "#{node['serverspec']['dest_spec_path']}/spec" do
   source "#{node['serverspec']['user_spec_path']}"
   action :create
 end
