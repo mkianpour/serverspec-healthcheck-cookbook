1f4d04d1a72f3d6448d4ffbc4296853dec89dcdf
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 11:30:09 +0200

    install serverspec modified

--- recipes/serverspec_install.rb
+++ recipes/serverspec_install.rb
@@ -1,12 +1,8 @@
-
-execute 'install_bundler' do
-  command 'sudo gem install bundler'
-end
-
 bash 'install_serverspec' do
   code <<-EOH
     eval "$(/root/.rbenv/bin/rbenv init -)";
-    sudo gem install serverspec
+    gem install bundler
+    gem install serverspec
     EOH
 end
 
