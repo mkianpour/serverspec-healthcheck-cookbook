b7cd8dadb6bcdcf316a60cd9bc7e07463558bb15
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 13:44:27 +0200

    serverspec init in /root

--- recipes/serverspec_install.rb
+++ recipes/serverspec_install.rb
@@ -3,10 +3,9 @@ bash 'install_serverspec' do
     eval "$(/root/.rbenv/bin/rbenv init -)";
     gem install bundler
     gem install serverspec
-    serverspec-init /root
     EOH
 end
 
 execute 'init serverspec' do
-  command 'serverspec-init /root/'
+  command 'mkdir /root/serverspec;cd /root/serverspec;serverspec-init'
 end
