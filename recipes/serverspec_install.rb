4c494aa4fc42dd216d38665c1f07dcfbc54cbc54
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 11:11:13 +0200

    adding tests for serverspec

--- recipes/serverspec_install.rb
+++ recipes/serverspec_install.rb
@@ -1,11 +1,15 @@
 
 execute 'install_bundler' do
-  command 'gem install bundler'
+  command 'sudo gem install bundler'
 end
 
 bash 'install_serverspec' do
   code <<-EOH
     eval "$(/root/.rbenv/bin/rbenv init -)";
-    gem install serverspec
+    sudo gem install serverspec
     EOH
+end
+
+execute 'init serverspec' do
+  command 'serverspec-init .'
 end
---
+++ test/integration/default/rspec/serverspec_spec.rb
@@ -0,0 +1,14 @@
+# Using rspec as I don't want to test installation of
+# serverspec with serverspec! :)
+
+def check_serverspec
+  `sudo find /opt/chef -name 'serverspec'`
+end
+
+describe check_serverspec do
+  it { should include('serverspec', 'gems') }
+end
+
+describe 'serverspec_init_check' do
+  expect(File) exist('/home/root/Rakefile')
+end
