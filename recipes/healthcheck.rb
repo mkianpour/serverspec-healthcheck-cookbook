d6650ef7cd96ca61e8f3f618f068944bfc9a3f0e
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-18 16:16:58 +0200

    directory outside cookbook

--- recipes/healthcheck.rb
+++ recipes/healthcheck.rb
@@ -1,4 +1,4 @@
-remote_directory "#{node['serverspec']['dest_spec_path']}/spec" do
-  source "file:///#{node['serverspec']['user_spec_path']}/spec"
+directory "#{node['serverspec']['dest_spec_path']}/spec" do
+  path "#{node['serverspec']['user_spec_path']}/spec"
   action :create
 end
