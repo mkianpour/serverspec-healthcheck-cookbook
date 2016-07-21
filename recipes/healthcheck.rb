a13b83d1c423e83c56e8639e2f44f097244d75a1
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-21 16:42:06 +0200

    comment out rake

--- recipes/healthcheck.rb
+++ recipes/healthcheck.rb
@@ -10,7 +10,7 @@ cookbook_file "#{node['serverspec']['dest_spec_path']}/Rakefile" do
   action :create
 end
 
-execute 'sample specs' do
-  command "cd #{spec_path};rake spec"
-  only_if { ::File.exist?("#{spec_path}/spec") }
-end
+# execute 'sample specs' do
+#   command "cd #{spec_path};rake spec"
+#   only_if { ::File.exist?("#{spec_path}/spec") }
+# end
