3f74dc5c9fc9e34c86a1827476455fea4cdcb566
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-21 15:28:00 +0200

    execute rake

--- recipes/serverspec_install.rb
+++ recipes/serverspec_install.rb
@@ -8,11 +8,6 @@ end
 
 spec_path = node['serverspec']['dest_spec_path']
 
-# execute 'sample specs' do
-#   command "mkdir #{spec_path};cd #{spec_path}"
-#   not_if { ::File.exist?("#{spec_path}/spec") }
-# end
-
 remote_directory "#{node['serverspec']['dest_spec_path']}/spec" do
   source "#{node['serverspec']['user_spec_path']}"
   action :create
@@ -21,4 +16,9 @@ end
 cookbook_file "#{node['serverspec']['dest_spec_path']}/Rakefile" do
   source "#{node['serverspec']['user_spec_path']}/Rakefile"
   action :create
+end
+
+execute 'sample specs' do
+  command "cd #{spec_path};rake spec"
+  not_if { ::File.exist?("#{spec_path}/spec") }
 end
